2476c0be57f949f3aa7dc13cebf747f942308c5e
Eugeny1978 <luchnik78@mail.ru>
2024-04-03 11:29:01 +0300

    Nodify Files

--- tests.py
+++ tests.py
@@ -1,17 +1,31 @@
-import json
-#
+## Проверка на Число (только на целое если float - то через try:)
 # data = '3456.dd'
-#
 # num = data.split('.')[0]
 # print(num.isnumeric())
 # print(num.isdigit())
 # print(num.isdecimal())
 # print(float(data).isdecimal())
 
-path_file_cenz = r'common/cenz.json'
-# cenzored_words = set(json.load(open(path_file_cenz)))
-cenzored_words = set(json.load(open(path_file_cenz))).intersection()
-print(cenzored_words)
+## Открыть JSON Файл -------------------
+# import json
+# path_file_cenz = r'common/cenz.json'
+# # cenzored_words = set(json.load(open(path_file_cenz)))
+# cenzored_words = set(json.load(open(path_file_cenz))).intersection()
+# print(cenzored_words)
+
+## Свой вариант пробовал через БД ------------------
+# import sqlite3 as sq
+# DATABASE = 'DATABASE.db'
+# NAME_BOT = 'Trades'
+# USERNAME_BOT = 'Trades_mm_bot'#
+# def get_token():
+#     with sq.connect(DATABASE) as connect:
+#         # connect.row_factory = sq.Row
+#         curs = connect.cursor()
+#         curs.execute(f"SELECT token FROM Tokens WHERE username IS '{USERNAME_BOT}'")
+#         return curs.fetchone()[0]
+# if __name__ == '__main__':#
+#     print(get_token())
 
 
 
